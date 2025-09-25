0aac0bcafbb5b1ccc778089be2bbce7e23844774
mysticmythical254-lab <mysticmythical254@gmail.com>
2025-09-25 23:22:57 +0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/10.3.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.3.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/favicon.ico'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
